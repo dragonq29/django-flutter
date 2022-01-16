b1be0d4610d71464a6ef1bdf840506d6b0ca7346
DragonQ <dragonq29@gmail.com>
2022-01-16 21:49:51 +0900

    settings에 rest_framework 등록 추가

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'plan'
+    'plan',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
