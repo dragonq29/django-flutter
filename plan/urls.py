d9c3c2a35ea5770dc00b383317bfdb61945e3bb6
DragonQ <dragonq29@gmail.com>
2022-01-16 22:35:53 +0900

    최신 DRF 반영(basename이 있어야 하고, 슬레쉬도 있어야 함)

--- plan/urls.py
+++ plan/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 router = routers.DefaultRouter(trailing_slash=False)
-router.register('plan', views.TodoView)
+router.register(r'plan/', views.TodoView, basename='plan')
 urlpatterns = [
     path('', include(router.urls)),
 ]
