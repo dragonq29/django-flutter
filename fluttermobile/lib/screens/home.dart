45c5400b549165654ce135b573ef1e52dd324d9e
DragonQ <dragonq29@gmail.com>
2022-01-23 16:33:51 +0900

    home.dart 추가

---
+++ fluttermobile/lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('운명을 바꾸는 버킷리스트')),
+
+    );
+  }
+}
